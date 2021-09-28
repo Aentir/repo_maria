d1c93a4a5041a772a1726aa7f97e777a3f3556f5
kenny Callado <kennycallado@hotmail.com>
2021-09-28 15:49:46 +0200

    # Coimmit 2       - Modifica florida.sql         - Agrega, crear base de datos si no existe         - Agrega, uso de la base de datos       - Modifica inex.php         - Maneja lógica de vista         - si recibe  lo gestiona       - Modifica Info.php         - Metodo get recibe parámetro, null default         - Si recibe parámetro, imprime (echo) registro obtenido.         - Por defecto muestra la lista completa.

--- index.php
+++ index.php
@@ -4,32 +4,37 @@ include_once 'clases/Info.php';
 
 $info = new Info();
 
+// Creo que el isset es lo que te faltaba para que no diera error.
+$search = isset($_GET["alum_nombre"]) ? $_GET["alum_nombre"] : "";
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-    <form action="" method="get">
-        <label>Nombre a buscar: </label>
-            <input type="text" name="alum_nombre">
-            <input type="submit" name="enviando" value="Enviar">
-    </form>
-    <thead>
-        <tr>Nombre</tr>
-        <tr>Apellido</tr>
-        <tr>Apellido2</tr>
-        <tr>DNI</tr>
-        <br>
-    </thead>
-    <tbody>
-        <?=$info->getInfo()?>
-    </tbody>
-    
+  <form action="" method="get">
+    <label>Nombre a buscar: </label>
+    <input type="text" name="alum_nombre">
+    <input type="submit" name="enviando" value="Enviar">
+  </form>
+  <thead>
+    <tr>Nombre</tr>
+    <tr>Apellido</tr>
+    <tr>Apellido2</tr>
+    <tr>DNI</tr>
+    <br>
+  </thead>
+  <tbody>
+    <?= $info->getInfo($search) ?>
+  </tbody>
+
 </body>
+
 </html>
